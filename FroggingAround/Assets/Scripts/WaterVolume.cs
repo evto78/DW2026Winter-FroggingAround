c5eef3bff867c3875804105e566c824438cf154f
evto78 <150751428+evto78@users.noreply.github.com>
2026-03-05 15:49:38 -0500

    Added more props

--- FroggingAround/Assets/Scripts/WaterVolume.cs
+++ FroggingAround/Assets/Scripts/WaterVolume.cs
@@ -12,15 +12,17 @@ public class WaterVolume : MonoBehaviour
         {
             if(other.gameObject.tag == "Fly")
             {
-                rb.AddForce(Vector3.up * (objDepth*1.5f) * (boyncy / 4f) * Time.deltaTime);
+                rb.AddForce(Vector3.up * (objDepth*1.5f) * (boyncy / 4f) * Time.deltaTime );
             }
             else
             {
                 rb.AddForce(Vector3.up * (objDepth*2f) * boyncy * Time.deltaTime);
             }
+            if(rb.velocity.y > 3f && other.gameObject.tag != "Player") { rb.velocity = new Vector3(rb.velocity.x, 3f, rb.velocity.z); }
         }
         else if (other.gameObject.GetComponentInParent<Rigidbody>() != null)
         {
+
             if (other.gameObject.tag == "Fly")
             {
                 other.gameObject.GetComponentInParent<Rigidbody>().AddForce(Vector3.up * (objDepth*1.5f) * (boyncy / 4f) * Time.deltaTime);
@@ -29,6 +31,8 @@ public class WaterVolume : MonoBehaviour
             {
                 other.gameObject.GetComponentInParent<Rigidbody>().AddForce(Vector3.up * (objDepth*2f) * boyncy * Time.deltaTime);
             }
+            if (other.gameObject.GetComponentInParent<Rigidbody>().velocity.y > 3f && other.gameObject.tag != "Player") 
+            { other.gameObject.GetComponentInParent<Rigidbody>().velocity = new Vector3(other.gameObject.GetComponentInParent<Rigidbody>().velocity.x, 3f, other.gameObject.GetComponentInParent<Rigidbody>().velocity.z); }
         }
     }
 }
